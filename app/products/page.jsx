a7673a075ffa303daa152b23559637b157b37230
Your Name <anjalisuthar114@gmail.com>
2026-05-08 13:43:15 +0530

    testing

--- app/products/page.jsx
+++ app/products/page.jsx
@@ -1,11 +1,13 @@
 import { Suspense } from "react";
 import ProductsClient from "../../components/ProductsClient";
+import Sidebar from "../../components/seller/Sidebar";
 
 
 export default function Page() {
   return (
     <Suspense fallback={<div>Loading...</div>}>
      <ProductsClient/>
+     <Sidebar/>
     </Suspense>
   );
 }
